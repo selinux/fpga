be22ade26c0d5c12d0cab1f332d86c53af5a374b
Julian Arnold <julian.arnold@ettus.com>
2015-02-17 16:09:24 -0800

    ofdm: noc_block_schmidl_cox sim lib support

--- usrp3/lib/rfnoc/noc_block_schmidl_cox.v
+++ usrp3/lib/rfnoc/noc_block_schmidl_cox.v
@@ -13,20 +13,25 @@ module noc_block_schmidl_cox #(
   output [63:0] debug
 );
 
-  /////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
   //
   // RFNoC Shell
   //
   ////////////////////////////////////////////////////////////
+  localparam SR_READBACK = 255;
+
   wire [31:0] set_data;
   wire [7:0]  set_addr;
   wire        set_stb;
+  reg  [63:0] rb_data;
 
   wire [63:0] cmdout_tdata, ackin_tdata;
   wire        cmdout_tlast, cmdout_tvalid, cmdout_tready, ackin_tlast, ackin_tvalid, ackin_tready;
 
   wire [63:0] str_sink_tdata, str_src_tdata;
   wire        str_sink_tlast, str_sink_tvalid, str_sink_tready, str_src_tlast, str_src_tvalid, str_src_tready;
+
+  wire        clear_tx_seqnum;
 
   noc_shell #(
     .NOC_ID(NOC_ID),
@@ -46,19 +51,77 @@ module noc_block_schmidl_cox #(
     .str_sink_tdata(str_sink_tdata), .str_sink_tlast(str_sink_tlast), .str_sink_tvalid(str_sink_tvalid), .str_sink_tready(str_sink_tready),
     // Stream Source
     .str_src_tdata(str_src_tdata), .str_src_tlast(str_src_tlast), .str_src_tvalid(str_src_tvalid), .str_src_tready(str_src_tready),
+    .clear_tx_seqnum(clear_tx_seqnum),
     .debug(debug));
 
-  // Control Source Unused
-  assign cmdout_tdata = 64'd0;
-  assign cmdout_tlast = 1'b0;
-  assign cmdout_tvalid = 1'b0;
-  assign ackin_tready = 1'b1;
+  ////////////////////////////////////////////////////////////
+  //
+  // AXI Wrapper
+  // Convert RFNoC Shell interface into AXI stream interface
+  //
+  ////////////////////////////////////////////////////////////
+  localparam NUM_AXI_CONFIG_BUS = 1;
+  
+  wire [31:0] m_axis_data_tdata;
+  wire        m_axis_data_tlast;
+  wire        m_axis_data_tvalid;
+  wire        m_axis_data_tready;
+  
+  wire [31:0] s_axis_data_tdata;
+  wire        s_axis_data_tlast;
+  wire        s_axis_data_tvalid;
+  wire        s_axis_data_tready;
+  
+  wire [31:0] m_axis_config_tdata;
+  wire        m_axis_config_tvalid;
+  wire        m_axis_config_tready;
+  
+  localparam AXI_WRAPPER_BASE    = 128;
+  localparam SR_NEXT_DST         = AXI_WRAPPER_BASE;
+  localparam SR_AXI_CONFIG_BASE  = AXI_WRAPPER_BASE + 1;
 
-   // FIXME this needs an axi_wrapper, it talks 32bit data
+  // Set next destination in chain
+  wire [15:0] next_dst;
+  setting_reg #(
+    .my_addr(SR_NEXT_DST), .width(16))
+  sr_next_dst(
+    .clk(ce_clk), .rst(ce_rst),
+    .strobe(set_stb), .addr(set_addr), .in(set_data), .out(next_dst), .changed());
+
+  axi_wrapper #(
+    .SR_AXI_CONFIG_BASE(SR_AXI_CONFIG_BASE),
+    .NUM_AXI_CONFIG_BUS(NUM_AXI_CONFIG_BUS))
+  inst_axi_wrapper (
+    .clk(ce_clk), .reset(ce_rst),
+    .clear_tx_seqnum(clear_tx_seqnum),
+    .next_dst(next_dst),
+    .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
+    .i_tdata(str_sink_tdata), .i_tlast(str_sink_tlast), .i_tvalid(str_sink_tvalid), .i_tready(str_sink_tready),
+    .o_tdata(str_src_tdata), .o_tlast(str_src_tlast), .o_tvalid(str_src_tvalid), .o_tready(str_src_tready),
+    .m_axis_data_tdata(m_axis_data_tdata),
+    .m_axis_data_tlast(m_axis_data_tlast),
+    .m_axis_data_tvalid(m_axis_data_tvalid),
+    .m_axis_data_tready(m_axis_data_tready),
+    .s_axis_data_tdata(s_axis_data_tdata),
+    .s_axis_data_tlast(s_axis_data_tlast),
+    .s_axis_data_tvalid(s_axis_data_tvalid),
+    .s_axis_data_tready(s_axis_data_tready),
+    .m_axis_config_tdata(m_axis_config_tdata),
+    .m_axis_config_tlast(),
+    .m_axis_config_tvalid(m_axis_config_tvalid), 
+    .m_axis_config_tready(m_axis_config_tready));
+  
+  ////////////////////////////////////////////////////////////
+  //
+  // User code
+  //
+  ////////////////////////////////////////////////////////////
+  
+
   schmidl_cox inst_schmidl_cox (
     .clk(ce_clk), .reset(ce_rst), .clear(1'b0),
     .set_stb(set_stb), .set_addr(set_addr), .set_data(set_data),
-    .i_tdata(str_sink_tdata), .i_tlast(str_sink_tlast), .i_tvalid(str_sink_tvalid), .i_tready(str_sink_tready),
-    .o_tdata(str_src_tdata), .o_tlast(str_src_tlast), .o_tvalid(str_src_tvalid), .o_tready(str_src_tready));
+    .i_tdata(m_axis_data_tdata), .i_tlast(m_axis_data_tlast), .i_tvalid(m_axis_data_tvalid), .i_tready(m_axis_data_tready),
+    .o_tdata(s_axis_data_tdata), .o_tlast(s_axis_data_tlast), .o_tvalid(s_axis_data_tvalid), .o_tready(s_axis_data_tready));
 
 endmodule
---
+++ usrp3/lib/rfnoc/noc_block_schmidl_cox_tb/noc_block_schmidl_cox_tb.sv
@@ -0,0 +1,52 @@
+//
+// Copyright 2014 Ettus Research LLC
+//
+`timescale 1ns/1ps
+
+module noc_block_schmidl_cox_tb();
+  // rfnoc_sim_lib options
+  `define NUM_CE 1          // Redefined to correct value
+  `define RFNOC_SIM_LIB_INC_AXI_WRAPPER
+  `define SIMPLE_MODE 0
+  `include "rfnoc_sim_lib.v" 
+
+  /*********************************************
+  ** DUT
+  *********************************************/
+  noc_block_schmidl_cox inst_noc_block_schmidl_cox (
+    .bus_clk(bus_clk), .bus_rst(bus_rst),
+    .ce_clk(ce_clk), .ce_rst(ce_rst),
+    .i_tdata(ce_o_tdata[0]), .i_tlast(ce_o_tlast[0]), .i_tvalid(ce_o_tvalid[0]), .i_tready(ce_o_tready[0]),
+    .o_tdata(ce_i_tdata[0]), .o_tlast(ce_i_tlast[0]), .o_tvalid(ce_i_tvalid[0]), .o_tready(ce_i_tready[0]),
+    .debug());
+
+  localparam [ 3:0] SCHMIDL_COX_XBAR_PORT = 4'd0;
+  localparam [15:0] SCHMIDL_COX_SID       = {XBAR_ADDR, SCHMIDL_COX_XBAR_PORT, 4'd0};
+  
+  integer i;
+  reg [31:0] payload = 32'd0;
+
+  initial begin
+    @(negedge ce_rst);
+    @(posedge ce_clk);
+    #1000;
+    // Setup Vector IIR
+    SendCtrlPacket(SCHMIDL_COX_SID, {24'd0, SR_FLOW_CTRL_PKTS_PER_ACK_BASE, 32'h8000_0001});                  // Command packet to set up flow control
+    SendCtrlPacket(SCHMIDL_COX_SID, {24'd0, SR_FLOW_CTRL_WINDOW_SIZE_BASE, 32'h0000_0FFF});                   // Command packet to set up source control window size
+    SendCtrlPacket(SCHMIDL_COX_SID, {24'd0, SR_FLOW_CTRL_WINDOW_EN_BASE, 32'h0000_0001});                     // Command packet to set up source control window enable
+    SendCtrlPacket(SCHMIDL_COX_SID, {24'd0, SR_NEXT_DST_BASE, {16'd0, TESTBENCH_SID}});                       // Set next destination
+    // periodic framer settings
+    SendCtrlPacket(SCHMIDL_COX_SID, {32'h10, 32'd64});  // frame_len  (FFTsize)
+    SendCtrlPacket(SCHMIDL_COX_SID, {32'h11, 32'd16});  // gap_len  (CP)
+    SendCtrlPacket(SCHMIDL_COX_SID, {32'h12, 32'd20}); // time offset to first frame
+    SendCtrlPacket(SCHMIDL_COX_SID, {32'h13, 32'd12});  // default max number of frames
+    #1000;
+
+    tb_next_dst = SCHMIDL_COX_SID;
+  end
+  
+  
+
+  // Assertions
+
+endmodule
