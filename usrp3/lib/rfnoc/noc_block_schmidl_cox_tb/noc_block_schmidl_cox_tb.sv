073abda9fd5d60728bc8ade3b6bcc94b28b5e54f
Julian Arnold <julian.arnold@ettus.com>
2015-03-25 15:06:01 -0700

    ofdm: adjusted timing to account for pipelining

--- usrp3/lib/rfnoc/noc_block_schmidl_cox_tb/noc_block_schmidl_cox_tb.sv
+++ usrp3/lib/rfnoc/noc_block_schmidl_cox_tb/noc_block_schmidl_cox_tb.sv
@@ -83,7 +83,7 @@ module noc_block_schmidl_cox_tb();
     // periodic framer settings
     SendCtrlPacket(SCHMIDL_COX_SID, {32'h10, 32'd64});  // frame_len  (FFTsize)
     SendCtrlPacket(SCHMIDL_COX_SID, {32'h11, 32'd16});  // gap_len  (CP)
-    SendCtrlPacket(SCHMIDL_COX_SID, {32'h12, 32'd29});  // time offset to first frame
+    SendCtrlPacket(SCHMIDL_COX_SID, {32'h12, 32'd22});  // time offset to first frame
     SendCtrlPacket(SCHMIDL_COX_SID, {32'h13, 32'd12});  // default max number of frames
     SendCtrlPacket(SCHMIDL_COX_SID, {32'h14, 32'd0});   // not used. set to 0
     
