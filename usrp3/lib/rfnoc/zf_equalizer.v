5a9cd859045f8b8c95dba6b8b2214fcaf761fb76
Julian Arnold <julian.arnold@ettus.com>
2015-03-25 14:41:28 -0700

    ofdm: modified code delays

--- usrp3/lib/rfnoc/zf_equalizer.v
+++ usrp3/lib/rfnoc/zf_equalizer.v
@@ -10,66 +10,100 @@ module zf_equalizer(input clk, input reset, input clear,
     localparam signed MAX_POS_VALUE = 16'd32767;
     localparam signed MAX_NEG_VALUE = -16'd32767;
     
+    wire do_op = 0;
+    
+    axi_fifo #(.SIZE(52)) int_axi_fifo_channel
+    
     //1 1 -1 -1 1 1 -1 1 -1 1 1 1 1 1 1 -1 -1 1 1 -1 1 -1 1 1 1 1 0 1 -1 -1 1 1 -1 1 -1 1 -1 -1 -1 -1 -1 1 1 -1 -1 1 -1 1 -1 1 1 1 1
-    reg signed [31:0] lp_rom [51:0];
+    reg signed [31:0] long_preamble_rom [52:0];
     
     initial()
+    begin
+    	long_preamble_rom[0] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[1] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[2] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[3] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[4] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[5] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[6] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[7] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[8] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[9] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[10] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[11] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[12] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[13] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[14] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[15] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[16] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[17] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[18] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[19] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[20] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[21] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[22] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[23] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[24] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[25] = {MAX_POS_VALUE, 16'b0};
+    	
+    	long_preamble_rom[26] = {16'b0, 16'b0};
+    	
+    	long_preamble_rom[27] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[28] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[29] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[30] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[31] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[32] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[33] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[34] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[35] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[36] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[37] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[38] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[39] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[40] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[41] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[42] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[43] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[44] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[45] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[46] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[47] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[48] = {MAX_NEG_VALUE, 16'b0};
+    	long_preamble_rom[49] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[50] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[51] = {MAX_POS_VALUE, 16'b0};
+    	long_preamble_rom[52] = {MAX_POS_VALUE, 16'b0};
+    end
+    
+    @always(posedge (clk))
+    begin
+    if(reset | clear)   
+        begin
+                
+        end
+   	else
     	begin
-    	lp[0] = {MAX_POS_VALUE, 16'b0};
-    	lp[1] = {MAX_POS_VALUE, 16'b0};
-    	lp[2] = {MAX_NEG_VALUE, 16'b0};
-    	lp[3] = {MAX_NEG_VALUE, 16'b0};
-    	lp[4] = {MAX_POS_VALUE, 16'b0};
-    	lp[5] = {MAX_POS_VALUE, 16'b0};
-    	lp[6] = {MAX_NEG_VALUE, 16'b0};
-    	lp[7] = {MAX_POS_VALUE, 16'b0};
-    	lp[8] = {MAX_NEG_VALUE, 16'b0};
-    	lp[9] = {MAX_POS_VALUE, 16'b0};
-    	lp[10] = {MAX_POS_VALUE, 16'b0};
-    	lp[11] = {MAX_POS_VALUE, 16'b0};
-    	lp[12] = {MAX_POS_VALUE, 16'b0};
-    	lp[13] = {MAX_POS_VALUE, 16'b0};
-    	lp[14] = {MAX_POS_VALUE, 16'b0};
-    	lp[15] = {MAX_NEG_VALUE, 16'b0};
-    	lp[16] = {MAX_NEG_VALUE, 16'b0};
-    	lp[17] = {MAX_POS_VALUE, 16'b0};
-    	lp[18] = {MAX_POS_VALUE, 16'b0};
-    	lp[19] = {MAX_NEG_VALUE, 16'b0};
-    	lp[20] = {MAX_POS_VALUE, 16'b0};
-    	lp[21] = {MAX_NEG_VALUE, 16'b0};
-    	lp[22] = {MAX_POS_VALUE, 16'b0};
-    	lp[23] = {MAX_POS_VALUE, 16'b0};
-    	lp[24] = {MAX_POS_VALUE, 16'b0};
-    	lp[25] = {MAX_POS_VALUE, 16'b0};
+        if(do_op)
+        	begin
+        	case(state)
+				ST_WAIT_FOR_FRAME :
+					begin
+						
+					end
+				ST_ESTIMATE :
+					begin
+						
+					end
+				ST_EQUALIZE : 
+					begin
+					
+					end		
+			end
+        end
+    end
     	
-    	lp[26] = {16'b0, 16'b0};
-    	
-    	lp[27] = {MAX_POS_VALUE, 16'b0};
-    	lp[28] = {MAX_NEG_VALUE, 16'b0};
-    	lp[29] = {MAX_NEG_VALUE, 16'b0};
-    	lp[30] = {MAX_POS_VALUE, 16'b0};
-    	lp[31] = {MAX_POS_VALUE, 16'b0};
-    	lp[32] = {MAX_NEG_VALUE, 16'b0};
-    	lp[33] = {MAX_POS_VALUE, 16'b0};
-    	lp[34] = {MAX_NEG_VALUE, 16'b0};
-    	lp[35] = {MAX_POS_VALUE, 16'b0};
-    	lp[36] = {MAX_NEG_VALUE, 16'b0};
-    	lp[37] = {MAX_NEG_VALUE, 16'b0};
-    	lp[38] = {MAX_NEG_VALUE, 16'b0};
-    	lp[39] = {MAX_NEG_VALUE, 16'b0};
-    	lp[40] = {MAX_NEG_VALUE, 16'b0};
-    	lp[41] = {MAX_POS_VALUE, 16'b0};
-    	lp[42] = {MAX_POS_VALUE, 16'b0};
-    	lp[43] = {MAX_NEG_VALUE, 16'b0};
-    	lp[44] = {MAX_NEG_VALUE, 16'b0};
-    	lp[45] = {MAX_POS_VALUE, 16'b0};
-    	lp[46] = {MAX_NEG_VALUE, 16'b0};
-    	lp[47] = {MAX_POS_VALUE, 16'b0};
-    	lp[48] = {MAX_NEG_VALUE, 16'b0};
-    	lp[49] = {MAX_POS_VALUE, 16'b0};
-    	lp[50] = {MAX_POS_VALUE, 16'b0};
-    	lp[51] = {MAX_POS_VALUE, 16'b0};
-    	lp[52] = {MAX_POS_VALUE, 16'b0};
-    	end
+    
+    
 
 endmodule
