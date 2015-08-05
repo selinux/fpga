c5f9da50a5e6c4ad7cbb4b65198eb215698970bb
Johannes Walter <johannes.walter@ettus.com>
2015-08-04 17:56:12 -0700

    rfnoc: 1-tap Equalizer

--- usrp3/lib/rfnoc/complex_invert.v
+++ usrp3/lib/rfnoc/complex_invert.v
@@ -14,10 +14,12 @@ module complex_invert
   output [31:0] o_tdata, output o_tlast, output o_tvalid, input  o_tready);
 
   wire [15:0] a_tdata;
+  wire [31:0] a_tdata_int;
   wire        a_tlast;
   wire        a_tvalid;
   wire        a_tready;
   wire [15:0] b_tdata;
+  wire [31:0] b_tdata_int;
   wire        b_tlast;
   wire        b_tvalid;
   wire        b_tready;
@@ -29,15 +31,18 @@ module complex_invert
   // Replicate input data into three streams with FIFOing to account for varying latency on the paths
   split_stream_fifo #(
     .WIDTH(32),
-    .ACTIVE_MASK(4'b0001),
+    .ACTIVE_MASK(4'b0111),
     .FIFOSIZE(5))
-  input_split_stream_fifo (
+  input_split_stream_fifo0 (
     .clk(clk), .reset(reset), .clear(clear),
     .i_tdata(i_tdata), .i_tlast(i_tlast), .i_tvalid(i_tvalid), .i_tready(i_tready),
-    .o0_tdata({a_tdata, 16'd0}), .o0_tlast(a_tlast), .o0_tvalid(a_tvalid), .o0_tready(a_tready),
-    .o1_tdata({16'd0, b_tdata}), .o1_tlast(b_tlast), .o1_tvalid(b_tvalid), .o2_tready(b_tready),
+    .o0_tdata(a_tdata_int), .o0_tlast(a_tlast), .o0_tvalid(a_tvalid), .o0_tready(a_tready),
+    .o1_tdata(b_tdata_int), .o1_tlast(b_tlast), .o1_tvalid(b_tvalid), .o1_tready(b_tready),
     .o2_tdata(a_b_tdata), .o2_tlast(a_b_tlast), .o2_tvalid(a_b_tvalid), .o2_tready(a_b_tready),
     .o3_tdata(), .o3_tlast(), .o3_tvalid(), .o3_tready(1'b0));
+
+  assign a_tdata = a_tdata_int[31:16];
+  assign b_tdata = b_tdata_int[15:0];
 
   wire [31:0] a2_plus_b2_tdata;
   wire        a2_plus_b2_tlast;
@@ -45,7 +50,7 @@ module complex_invert
   wire        a2_plus_b2_tready;
 
   // a^2 + b^2
-  complex_to_magsq 
+  complex_to_magsq
   a2_p_b2_complex_to_magsq (
     .clk(clk), .reset(reset), .clear(clear),
     .i_tdata(a_b_tdata), .i_tlast(a_b_tlast), .i_tvalid(a_b_tvalid), .i_tready(a_b_tready),
@@ -65,15 +70,16 @@ module complex_invert
     .WIDTH(32),
     .ACTIVE_MASK(4'b0011),
     .FIFOSIZE(5))
-  input_split_stream_fifo (
+  input_split_stream_fifo1 (
     .clk(clk), .reset(reset), .clear(clear),
     .i_tdata(a2_plus_b2_tdata), .i_tlast(a2_plus_b2_tlast), .i_tvalid(a2_plus_b2_tvalid), .i_tready(a2_plus_b2_tready),
     .o0_tdata(a2_plus_b2_0_tdata), .o0_tlast(a2_plus_b2_0_tlast), .o0_tvalid(a2_plus_b2_0_tvalid), .o0_tready(a2_plus_b2_0_tready),
-    .o1_tdata(a2_plus_b2_1_tdata), .o1_tlast(a2_plus_b2_1_tlast), .o1_tvalid(a2_plus_b2_1_tvalid), .o2_tready(a2_plus_b2_1_tready),
+    .o1_tdata(a2_plus_b2_1_tdata), .o1_tlast(a2_plus_b2_1_tlast), .o1_tvalid(a2_plus_b2_1_tvalid), .o1_tready(a2_plus_b2_1_tready),
     .o2_tdata(), .o2_tlast(), .o2_tvalid(), .o2_tready(1'b0),
     .o3_tdata(), .o3_tlast(), .o3_tvalid(), .o3_tready(1'b0));
 
-  wire [47:0] a_div_a2_plus_b2_tdata; // Q16.32, signed, 16 integer bits, 32 fraction
+  wire [47:0] a_div_a2_plus_b2_tdata_int; // Q16.32, signed, 16 integer bits, 32 fraction
+  wire [46:0] a_div_a2_plus_b2_tdata = {a_div_a2_plus_b2_tdata_int[47:32], a_div_a2_plus_b2_tdata_int[30:0]};
   wire        a_div_a2_plus_b2_tlast;
   wire        a_div_a2_plus_b2_tvalid;
   wire        a_div_a2_plus_b2_tready;
@@ -86,42 +92,43 @@ module complex_invert
     .aclk(clk), .aresetn(~reset),
     .s_axis_divisor_tdata(a2_plus_b2_0_tdata), .s_axis_divisor_tlast(a2_plus_b2_0_tlast), .s_axis_divisor_tvalid(a2_plus_b2_0_tvalid), .s_axis_divisor_tready(a2_plus_b2_0_tready),
     .s_axis_dividend_tdata(a_tdata), .s_axis_dividend_tlast(a_tlast), .s_axis_dividend_tvalid(a_tvalid), .s_axis_dividend_tready(a_tready),
-    .m_axis_dout_tdata(a_div_a2_plus_b2_tdata), .m_axis_dout_tlast(a_div_a2_plus_b2_tlast), .m_axis_dout_tvalid(a_div_a2_plus_b2_tvalid), .m_axis_dout_tready(a_div_a2_plus_b2_tready));
+    .m_axis_dout_tdata(a_div_a2_plus_b2_tdata_int), .m_axis_dout_tlast(a_div_a2_plus_b2_tlast), .m_axis_dout_tvalid(a_div_a2_plus_b2_tvalid), .m_axis_dout_tready(a_div_a2_plus_b2_tready));
 
   wire [15:0] neg_b_tdata;
   wire        neg_b_tlast;
   wire        neg_b_tvalid;
   wire        neg_b_tready;
-  wire        neg_b = (b_tdata == -16'sd32768) ? 16'sd32767 : (~b_tdata + 1'b1);
+  wire [15:0] neg_b = (b_tdata == -16'sd32768) ? 16'sd32767 : (~b_tdata + 1'b1);
 
   // Negate b
-  axi_fifo_flop #(.WIDTH(17)) 
+  axi_fifo_flop #(.WIDTH(17))
   neg_b_axi_fifo_flop (
     .clk(clk), .reset(reset), .clear(clear),
     .i_tdata({b_tlast,neg_b}), .i_tvalid(b_tvalid), .i_tready(b_tready),
     .o_tdata({neg_b_tlast,neg_b_tdata}), .o_tvalid(neg_b_tvalid), .o_tready(neg_b_tready),
     .space(), .occupied());
 
-  wire [47:0] neg_b_div_a2_plus_b2_tdata;
+  wire [47:0] neg_b_div_a2_plus_b2_tdata_int;
+  wire [46:0] neg_b_div_a2_plus_b2_tdata = {neg_b_div_a2_plus_b2_tdata_int[47:32], neg_b_div_a2_plus_b2_tdata_int[30:0]};
   wire        neg_b_div_a2_plus_b2_tlast;
   wire        neg_b_div_a2_plus_b2_tvalid;
   wire        neg_b_div_a2_plus_b2_tready;
 
   //     bi
   // ---------
-  // a^2 + b^2 
+  // a^2 + b^2
   divide_int16_int32
   neg_b_div_a2_plus_b2_divider (
     .aclk(clk), .aresetn(~reset),
     .s_axis_divisor_tdata(a2_plus_b2_1_tdata), .s_axis_divisor_tlast(a2_plus_b2_1_tlast), .s_axis_divisor_tvalid(a2_plus_b2_1_tvalid), .s_axis_divisor_tready(a2_plus_b2_1_tready),
     .s_axis_dividend_tdata(neg_b_tdata), .s_axis_dividend_tlast(neg_b_tlast), .s_axis_dividend_tvalid(neg_b_tvalid), .s_axis_dividend_tready(neg_b_tready),
-    .m_axis_dout_tdata(neg_b_div_a2_plus_b2_tdata), .m_axis_dout_tlast(neg_b_div_a2_plus_b2_tlast), .m_axis_dout_tvalid(neg_b_div_a2_plus_b2_tvalid), .m_axis_dout_tready(neg_b_div_a2_plus_b2_tready));
+    .m_axis_dout_tdata(neg_b_div_a2_plus_b2_tdata_int), .m_axis_dout_tlast(neg_b_div_a2_plus_b2_tlast), .m_axis_dout_tvalid(neg_b_div_a2_plus_b2_tvalid), .m_axis_dout_tready(neg_b_div_a2_plus_b2_tready));
 
-  wire [95:0] one_div_a_plus_bi_tdata = {a_div_a2_plus_b2_tdata,neg_b_div_a2_plus_b2_tdata}
+  wire [93:0] one_div_a_plus_bi_tdata = {a_div_a2_plus_b2_tdata,neg_b_div_a2_plus_b2_tdata};
   wire        one_div_a_plus_bi_tlast;
   wire        one_div_a_plus_bi_tvalid;
   wire        one_div_a_plus_bi_tready;
-  
+
   // Join into one word
   axi_join #(
     .INPUTS(2))
@@ -131,12 +138,12 @@ module complex_invert
 
   // Truncate to a complex int16
   axi_round_and_clip_complex #(
-    .WIDTH_IN(48),
+    .WIDTH_IN(47),
     .WIDTH_OUT(16),
-    .CLIP_BITS(4), // TODO: Adjust scaling here
+    .CLIP_BITS(16), // TODO: Adjust scaling here
     .FIFOSIZE())
   inst_axi_round_and_clip_complex (
-    .clk(), .reset(),
+    .clk(clk), .reset(reset),
     .i_tdata(one_div_a_plus_bi_tdata), .i_tlast(one_div_a_plus_bi_tlast), .i_tvalid(one_div_a_plus_bi_tvalid), .i_tready(one_div_a_plus_bi_tready),
     .o_tdata(o_tdata), .o_tlast(o_tlast), .o_tvalid(o_tvalid), .o_tready(o_tready));
 
