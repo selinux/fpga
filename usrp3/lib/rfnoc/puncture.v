63bcc12d7322bdb22bf4f24d2f8254abd6b762df
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-08-12 23:01:44 -0700

    rfnoc: puncture: Fixed off by one error on vector length
    - Caused incorrect starting point in puncture vector

--- usrp3/lib/rfnoc/puncture.v
+++ usrp3/lib/rfnoc/puncture.v
@@ -10,7 +10,7 @@ module puncture #(
   parameter DEFAULT_PUNCTURE_VECTOR = 8'hFF)   // Puncture vector reset value
 (
   input clk, input reset, input clear,
-  input [$clog2(MAX_LEN)-1:0] vector_len_tdata, input vector_len_tvalid, output vector_len_tready,
+  input [$clog2(MAX_LEN):0] vector_len_tdata, input vector_len_tvalid, output vector_len_tready,
   input [MAX_LEN-1:0] puncture_vector_tdata, input puncture_vector_tvalid, output puncture_vector_tready,
   input [WIDTH-1:0] i_tdata, input i_tlast, input i_tvalid, output i_tready,
   output [WIDTH-1:0] o_tdata, output o_tlast, output o_tvalid, input o_tready
@@ -19,19 +19,19 @@ module puncture #(
   assign vector_len_tready      = 1'b1;
   assign puncture_vector_tready = 1'b1;
 
-  reg [$clog2(MAX_LEN)-1:0] puncture_index, vector_len;
+  reg [$clog2(MAX_LEN):0] puncture_index, vector_len;
   reg [MAX_LEN-1:0] puncture_vector;
   always @(posedge clk) begin
     if (reset | clear) begin
-      vector_len         <= DEFAULT_VECTOR_LEN;
+      vector_len         <= DEFAULT_VECTOR_LEN-1;
+      puncture_index     <= DEFAULT_VECTOR_LEN-1;
       puncture_vector    <= DEFAULT_PUNCTURE_VECTOR;
-      puncture_index     <= 0;
     end else begin
       if (puncture_vector_tvalid | vector_len_tvalid) begin
-        puncture_index   <= vector_len - 1;
+        puncture_index   <= vector_len_tdata - 1;
       end else if (i_tready & i_tvalid) begin
         if (puncture_index == 0) begin
-          puncture_index <= vector_len - 1;
+          puncture_index <= vector_len;
         end else begin
           puncture_index <= puncture_index - 1;
         end
@@ -40,7 +40,7 @@ module puncture #(
         puncture_vector = puncture_vector_tdata;
       end
       if (vector_len_tvalid) begin
-        vector_len = vector_len_tdata;
+        vector_len = vector_len_tdata - 1;
       end
     end
   end
