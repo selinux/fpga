db61184f5ed1b7ae4631a130fd5f54f13f1feda7
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-08-14 15:46:49 -0700

    lib: axi_setting_reg: Fixed issue where REPEATS and VALID_AT_RESET did not work correctly when used together

--- usrp3/lib/control/axi_setting_reg.v
+++ usrp3/lib/control/axi_setting_reg.v
@@ -10,31 +10,26 @@ module axi_setting_reg #(
   parameter DATA_AT_RESET = 0,
   parameter VALID_AT_RESET = 0, // Assert tvalid at reset
   parameter REPEATS = 0,        // Continuously assert tvalid after first write
-  parameter MSB_ALIGN = 0       // Shift data to the left to be aligned with MSB
-)
+  parameter MSB_ALIGN = 0)      // Shift data to the left to be aligned with MSB
 (
   input clk, input reset, 
   input set_stb, input [AWIDTH-1:0] set_addr, input [31:0] set_data,
   output reg [WIDTH-1:0] o_tdata, output o_tlast, output reg o_tvalid, input o_tready
 );
 
-  reg init;
-
   always @(posedge clk) begin
     if (reset) begin
       o_tdata  <= DATA_AT_RESET;
       o_tvalid <= VALID_AT_RESET;
-      init <= 1'b0;
     end else begin
       if (set_stb & (ADDR==set_addr)) begin
-        init <= 1'b1;
         o_tdata  <= (MSB_ALIGN == 0) ? set_data[WIDTH-1:0] : set_data[31:32-WIDTH];
         o_tvalid <= 1'b1;
       end
       if (o_tvalid & o_tready) begin
         o_tvalid <= 1'b0;
       end
-      if (init & REPEATS) begin
+      if (o_tvalid & REPEATS) begin
         o_tvalid <= 1'b1;
       end
     end
