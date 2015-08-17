9c0fb98c2dab6a60c94f2813ff617c47c7186d33
Jonathon Pendlum <jonathon.pendlum@ettus.com>
2015-08-17 12:10:41 -0700

    rfnoc: Added ability to bypass output registers in AXI round and AXI clip

--- usrp3/lib/rfnoc/axi_round.v
+++ usrp3/lib/rfnoc/axi_round.v
@@ -8,7 +8,7 @@ module axi_round
     parameter round_to_zero=0,       // original behavior
     parameter round_to_nearest=1,    // lowest noise
     parameter trunc=0,               // round to negative infinity
-    parameter FIFOSIZE=0)  // leave at 0 for a normal single flop
+    parameter FIFOSIZE=1)            // FIFOSIZE = 1, single output register
    (input clk, input reset,
     input [WIDTH_IN-1:0] i_tdata, input i_tlast, input i_tvalid, output i_tready,
     output [WIDTH_OUT-1:0] o_tdata, output o_tlast, output o_tvalid, input o_tready);
@@ -34,10 +34,19 @@ module axi_round
 
    assign err = i_tdata - {out,{(WIDTH_IN-WIDTH_OUT){1'b0}}};
 
-   axi_fifo #(.WIDTH(WIDTH_OUT+1), .SIZE(FIFOSIZE)) flop
-     (.clk(clk), .reset(reset), .clear(1'b0),
-      .i_tdata({i_tlast, out}), .i_tvalid(i_tvalid), .i_tready(i_tready),
-      .o_tdata({o_tlast, o_tdata}), .o_tvalid(o_tvalid), .o_tready(o_tready),
-      .occupied(), .space());
+   generate
+     if (FIFOSIZE == 0) begin
+       assign o_tdata = out;
+       assign o_tlast = i_tlast;
+       assign o_tvalid = i_tvalid;
+       assign i_tready = o_tready;
+     end else begin
+       axi_fifo #(.WIDTH(WIDTH_OUT+1), .SIZE(FIFOSIZE)) flop
+         (.clk(clk), .reset(reset), .clear(1'b0),
+          .i_tdata({i_tlast, out}), .i_tvalid(i_tvalid), .i_tready(i_tready),
+          .o_tdata({o_tlast, o_tdata}), .o_tvalid(o_tvalid), .o_tready(o_tready),
+          .occupied(), .space());
+     end
+   endgenerate
 
 endmodule // axi_round
